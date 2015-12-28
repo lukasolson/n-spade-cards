346efdb21f3421dbd3da3ec674d8690df4b88504
Lukas Olson <olson.lukas@gmail.com>
2015-12-27 20:40:35 -0700

    Redo gh-pages

--- ng-touch/app/common/filters/aggregate-panel.js
+++
@@ -1,6 +0,0 @@
-angular.module("nSpadeCards").filter("aggregatePanel", function () {
-	return function (panels) {
-		if (_.uniq(panels).length === 1) return panels[0];
-		return "card";
-	};
-});
--- ng-touch/app/common/filters/cross-section.js
+++
@@ -1,9 +0,0 @@
-angular.module("nSpadeCards").filter("crossSection", function () {
-	return function (grids, row, col) {
-		var crossSection = [];
-		for (var i = 0; i < grids.length; i++) {
-			crossSection.push(grids[i][row][col]);
-		}
-		return crossSection;
-	};
-});
